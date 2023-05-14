b1fa38c13f114e20f4d41356c9c29526b7fff65e
Stas <example@gmail.com>
2023-05-14 16:46:34 +0200

    add my answer for 15

--- script.js
+++ script.js
@@ -30,15 +30,36 @@ let personalMovieDB = {
 //         'logan': '8.1'
 //     }
 
+// 3.1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+// 4) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+// отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+// возвращаем пользователя к вопросам опять
 
-let a = prompt (" One of the last movies did you watch?", " "),
-    b = +prompt (" How much would you rate it", " "),
-    c = prompt (" One of the last movies did you watch?", " "),
-    d = +prompt (" How much would you rate it", " ");
+let a,b;
+for (let i = 0; i < 2; i++) {
+    a = prompt (" One of the last movies did you watch?", " ");
+    b = +prompt (" How much would you rate it", " ");
+    personalMovieDB.movies[a] = b;
+}
 
-                   
- personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+// 5) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+if (personalMovieDB.count < 10 || personalMovieDB.count > 0) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 || personalMovieDB.count < 30 ) {
+    console.log("Вы класический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    document.write('Произошла ошибка');
+} 
+
+
+
+
 
 
 
